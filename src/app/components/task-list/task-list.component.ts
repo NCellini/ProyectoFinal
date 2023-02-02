3ab9c0fe1d3add31832f4be7e321fa269588b543
NCellini <ncellini@fi.uba.ar>
2023-02-02 20:34:37 -0300

    se agregan vistas de prueba

--- src/app/components/task-list/task-list.component.ts
+++ src/app/components/task-list/task-list.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ITask, Levels } from 'src/app/models/interfaces/Task.Interface';
 
 @Component({
   selector: 'app-task-list',
   templateUrl: './task-list.component.html',
   styleUrls: ['./task-list.component.scss']
 })
-export class TaskListComponent {
+export class TaskListComponent implements OnInit {
+
+  // to do: Reformular como una lista de tareas
+
+  task1: ITask = {
+    title: 'Task 1',
+    description: 'Description 1',
+    completed: false,
+    level: Levels.Info
+  }
+
+  task2: ITask = {
+    title: 'Task 2',
+    description: 'Description 2',
+    completed: true,
+    level: Levels.Urgent
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
 
 }
+
